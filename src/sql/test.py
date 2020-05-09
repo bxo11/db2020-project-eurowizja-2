aca4f48e374729a3cc2581738f31457708e30fea
KrzysiekJa <krzysiu@MacBook-Pro-Krzysiu.local>
2020-05-09 22:32:56 +0200

    Added test.py script with functionality for execiution of sql

---
+++ src/sql/test.py
@@ -0,0 +1,27 @@
+import os
+import pymysql
+
+
+def execute(SQL_string):
+    
+    connection = pymysql.Connect(
+        host = 'localhost',
+        user = os.getenv('root'),
+        db   = os.getenv('eurowizja'),
+        charset     = 'utf8mb4',
+        cursorclass = pymysql.cursors.DictCursor
+    )
+    result = None
+    
+    
+    with connection.cursor() as cursor:
+        
+        try:
+            cursor.execute(SQL_string)
+            result = cursor.fetchall()
+            
+    finally:
+        connection.close()
+    
+    
+    return result
